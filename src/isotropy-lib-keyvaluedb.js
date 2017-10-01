f51432a1173560ed19114f4693939bb259bbbb3f
arivanandan <uarivanandan@gmail.com>
2017-10-01 21:20:46 +0530

    Adds hash, set

--- src/isotropy-lib-keyvaluedb.js
+++ src/isotropy-lib-keyvaluedb.js
@@ -1,13 +1,41 @@
 import Redis from "ioredis";
 
+const objectFlattener = obj =>
+  Object.keys(obj).reduce((acc, cur) => acc.concat(cur).concat(obj[cur]), []);
+
+const pipeLiner = (redis, setKey, values, operation) => {
+  const pipeline = redis.pipeline();
+  if (operation === "hset")
+    Object.keys(values).forEach(key =>
+      pipeline[operation](setKey, key, values[key])
+    );
+  if (operation === "sadd")
+    values.forEach(value => pipeline[operation](setKey, value));
+  return pipeline.exec((err, result) => result);
+};
+
 export async function put(connStr, data) {
   const redis = new Redis(connStr);
-  return await redis.set(Object.keys(data)[0], Object.values(data)[0]);
+  return typeof data.value === "object"
+    ? data.value instanceof Array
+      ? await pipeLiner(redis, data.key, data.value, "sadd")
+      : await pipeLiner(redis, data.key, data.value, "hset")
+    : await redis.set(data.key, data.value);
 }
 
-export async function get(connStr, data) {
+export async function get(connStr, key) {
   const redis = new Redis(connStr);
-  return await redis.get(data);
+  return await redis.get(key);
+}
+
+export async function hget(connStr, hash, key) {
+  const redis = new Redis(connStr);
+  return await redis.hget(hash, key);
+}
+
+export async function sget(connStr, set) {
+  const redis = new Redis(connStr);
+  return await redis.smembers(set);
 }
 
 export async function del(connStr, data) {
--- src/test/test.js
+++ src/test/test.js
@@ -8,19 +8,51 @@ describe("lib-db", function() {
 
   it("put", async () => {
     const beforeInsert = await dbLib.exists("", "task");
-    await dbLib.put("", { task: "testTask" });
+    await dbLib.put("", { key: "task", value: "testTask" });
     const afterInsert = await dbLib.exists("", "task");
     beforeInsert.should.equal(0) && afterInsert.should.equal(1);
   });
 
+  it("put-hash", async () => {
+    const beforeInsert = await dbLib.exists("", "task");
+    await dbLib.put("", {
+      key: "task",
+      value: { type: "task", method: "just do it" }
+    });
+    const afterInsert = await dbLib.exists("", "task");
+    const afterInsertData = await dbLib.hget("", "task", "method");
+
+    beforeInsert.should.equal(0) &&
+      afterInsert.should.equal(1) &&
+      afterInsertData.should.equal("just do it");
+  });
+
+  it("put-set", async () => {
+    const beforeInsert = await dbLib.exists("", "task");
+    await dbLib.put("", {
+      key: "task",
+      value: ["Get Eggs", "Get Milk", "A wheel of cheese"]
+    });
+    const afterInsert = await dbLib.exists("", "task");
+    const afterInsertData = await dbLib.sget("", "task");
+
+    beforeInsert.should.equal(0) &&
+      afterInsert.should.equal(1) &&
+      afterInsertData.should.deepEqual([
+        "Get Milk",
+        "A wheel of cheese",
+        "Get Eggs"
+      ]);
+  });
+
   it("get", async () => {
-    await dbLib.put("", { task: "testTask" });
+    await dbLib.put("", { key: "task", value: "testTask" });
     const result = await dbLib.get("", "task");
     result.should.equal("testTask");
   });
 
   it("del", async () => {
-    await dbLib.put("", { task: "testTask" });
+    await dbLib.put("", { key: "task", value: "testTask" });
     const afterInsert = await dbLib.exists("", "task");
     await dbLib.del("", "task");
     const afterDelete = await dbLib.exists("", "task");
@@ -28,7 +60,7 @@ describe("lib-db", function() {
   });
 
   it("scan", async () => {
-    await dbLib.put("", { Get_Eggs: "Get liek 2" });
+    await dbLib.put("", { key: "Get_Eggs", value: "Get liek 2" });
     const dbPairs = await dbLib.scan("");
     dbPairs.should.deepEqual(["0", ["Get_Eggs"]]);
   });
